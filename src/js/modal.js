ae89cf352f138c0ab2c615251709ab2ebdf38039
s4tenko <s4tenko@gmail.com>
2021-05-07 09:45:35 +0300

    File distribution done

---
+++ src/js/modal.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const addArticle = document.querySelector(".main-btn");
+const modalWindow = document.querySelector(".modal-wrapper");
+const cancelBtn = document.querySelector(".modal-btn-cancel");
+const publishBtn = document.querySelector(".modal-btn-publish");
+const form = document.querySelector(".post-form");
+const articleTitle = document.querySelector(".modal-input-title");
+const articleContent = document.querySelector(".modal-input-content");
+
+addArticle.addEventListener("click", () => {
+  modalWindow.classList.toggle("modal-visible");
+});
+
+cancelBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  closeForm();
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modalWindow) {
+    closeForm();
+  }
+});
+
+articleTitle.addEventListener("change", () => {
+  articleTitle.classList.remove("modal-input-error");
+});
+
+articleContent.addEventListener("change", () => {
+  articleContent.classList.remove("modal-input-error");
+});
+
+publishBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!articleTitle.value) {
+    articleTitle.classList.add("modal-input-error");
+    return;
+  }
+  if (!articleContent.value) {
+    articleContent.classList.add("modal-input-error");
+    return;
+  }
+  const newArticle = {
+    name: articleTitle.value,
+    date: getCurrentDate(),
+    content: articleContent.value,
+  };
+  Data.push(newArticle);
+  searchInput.value = "";
+  renderArticles(searchInput.value);
+  closeForm();
+});
+
+function closeForm() {
+  form.reset();
+  articleTitle.classList.remove("modal-input-error");
+  articleContent.classList.remove("modal-input-error");
+  modalWindow.classList.toggle("modal-visible");
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,7 @@
 "use strict";
 
-const addArticle = document.querySelector(".main-btn");
-const modalWindow = document.querySelector(".modal-wrapper");
-const cancelBtn = document.querySelector(".modal-btn-cancel");
-const publishBtn = document.querySelector(".modal-btn-publish");
-const form = document.querySelector(".post-form");
-const articleTitle = document.querySelector(".modal-input-title");
-const articleContent = document.querySelector(".modal-input-content");
 const articlesWrapper = document.querySelector(".articles-wrapper");
 const searchInput = document.querySelector(".header-search-input");
-
-const editBtn = document.querySelectorAll(".btn-edit");
-const deleteBtn = document.querySelectorAll(".btn-delete"); 
 
 function renderArticles(searchStr) {
   if (searchStr) {
@@ -36,7 +26,7 @@ function createArticleWrapper(item) {
     "div",
     { classNames: ["main-article"] },
     articleHeader(item),
-    articleText(item),
+    articleText(item)
   );
 }
 
@@ -65,62 +55,10 @@ function articleText({ content }) {
   return textContent;
 }
 
-
 function getCurrentDate() {
   let today = new Date();
   return today.toLocaleDateString();
 }
-
-function closeForm() {
-  form.reset();
-  articleTitle.classList.remove("modal-input-error");
-  articleContent.classList.remove("modal-input-error");
-  modalWindow.classList.toggle("modal-visible");
-}
-
-addArticle.addEventListener("click", () => {
-  modalWindow.classList.toggle("modal-visible");
-});
-
-window.addEventListener("click", (e) => {
-  if (e.target == modalWindow) {
-    closeForm();
-  }
-});
-
-cancelBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  closeForm();
-});
-
-articleTitle.addEventListener("change", () => {
-  articleTitle.classList.remove("modal-input-error");
-});
-
-articleContent.addEventListener("change", () => {
-  articleContent.classList.remove("modal-input-error");
-});
-
-publishBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (!articleTitle.value) {
-    articleTitle.classList.add("modal-input-error");
-    return;
-  }
-  if (!articleContent.value) {
-    articleContent.classList.add("modal-input-error");
-    return;
-  }
-  const newArticle = {
-    name: articleTitle.value,
-    date: getCurrentDate(),
-    content: articleContent.value,
-  };
-  Data.push(newArticle);
-  searchInput.value = "";
-  renderArticles(searchInput.value);
-  closeForm();
-});
 
 searchInput.addEventListener("input", () => {
   renderArticles(searchInput.value);
@@ -145,12 +83,6 @@ function createElement(
 ) {
   const elem = document.createElement(tagName);
   elem.classList.add(...classNames);
-
-  // const attr = {
-  //   src: "htpps://example.com",
-  //   value: "text",
-  //   name: "textInput",
-  // };
 
   for (const [attrName, attrValue] of Object.entries(attributes)) {
     elem.setAttribute(attrName, attrValue);
